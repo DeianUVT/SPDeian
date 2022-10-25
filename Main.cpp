39e642aa7392fb69a40d1660e66691340bbabf25
DeianUVT <petar.pauta02@e-uvt.ro>
2022-10-25 16:18:01 +0300

    Lab 1 ckech and done

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,12 @@
 int main()
 {
     Book discoTitanic("Disco Titanic");
-discoTitanic.createNewParagraph("Paragraph 1");
-discoTitanic.createNewParagraph("Paragraph 2");
-discoTitanic.createNewParagraph("Paragraph 3");
-discoTitanic.createNewImage("Image 1");
-discoTitanic.createNewParagraph("Paragraph 4");
-discoTitanic.createNewTable("Table 1");
-discoTitanic.createNewParagraph("Paragraph 5");
+    discoTitanic.createNewParagraph("Paragraph 1");
+    discoTitanic.createNewParagraph("Paragraph 2");
+    discoTitanic.createNewParagraph("Paragraph 3");
+    discoTitanic.createNewImage("Image 1");
+    discoTitanic.createNewParagraph("Paragraph 4");
+    discoTitanic.createNewTable("Table 1");
+    discoTitanic.createNewParagraph("Paragraph 5");
+    discoTitanic.print();
 }
